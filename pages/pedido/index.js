509f4ce9ff38abe48bff36b9d7ae8a5fcf153d71
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-28 19:55:22 -0300

    feat: adding flavors into the order object

--- pages/pedido/index.js
+++ pages/pedido/index.js
@@ -6,60 +6,60 @@ function getValueFromDucument(id, isChecked = false) {
 
 const flavors = [
   {
-	name: "Selecione",
-	value: "",
+    name: "Selecione",
+    value: "",
   },
   {
-	name: "Strogonoff de Frango",
-	value: "chickenStrognoff",
+    name: "Strogonoff de Frango",
+    value: "chickenStrognoff",
   },
   {
-	name: "Calabresa",
-	value: "calabress",
+    name: "Calabresa",
+    value: "calabresa",
   },
   {
-	name: "Portuguesa",
-	value: "portuguesa",
+    name: "Portuguesa",
+    value: "portuguesa",
   },
   {
-	name: "Marguerita",
-	value: "margerita",
+    name: "Marguerita",
+    value: "margerita",
   },
   {
-	name: "Quatro queijos",
-	value: "fourCheeses",
+    name: "Quatro queijos",
+    value: "fourCheeses",
   },
   {
-	name: "Frango com Catupiry",
-	value: "chikenCatupity",
+    name: "Frango com Catupiry",
+    value: "chikenCatupity",
   },
   {
-	name: "Mexicana",
-	value: "mexican",
+    name: "Mexicana",
+    value: "mexican",
   },
   {
-	name: "Coração",
-	value: "hearth",
+    name: "Coração",
+    value: "hearth",
   },
   {
-	name: "Sensação",
-	value: "sensation",
+    name: "Sensação",
+    value: "sensation",
   },
   {
-	name: "Dois Amores",
-	value: "twoLoves",
+    name: "Dois Amores",
+    value: "twoLoves",
   },
   {
-	name: "Banana Nevada",
-	value: "white-banana",
+    name: "Banana Nevada",
+    value: "white-banana",
   },
   {
-	name: "Prestigio",
-	value: "prestigio",
+    name: "Prestigio",
+    value: "prestigio",
   },
   {
-	name: "Confete",
-	value: "confete",
+    name: "Confete",
+    value: "confete",
   },
 ];
 
@@ -69,108 +69,132 @@ document
   .getElementById("size")
   .addEventListener("change", createFlavorsOptions);
 
+let numberOfFlavors;
+
 function createFlavorsOptions() {
-
-	while(true){
-		const hasFlavors = document.getElementById("pizzaFlavors")
-		if(!!hasFlavors){
-			document.getElementById("pizzaFlavors").remove()
-		}
-		if(!hasFlavors) break
-	}
-
-  const pizzaSize = getValueFromDucument("size")
-
-  let numberOfFlavors;
-
-  switch(pizzaSize){
-	case "S": {
-		numberOfFlavors = 2
-		break
-	}
-	case "M": {
-		numberOfFlavors = 3
-		break
-	}
-	case "L": {
-		numberOfFlavors = 4
-		break
-	}
-	case "F": {
-		numberOfFlavors = 5
-		break
-	}
-	default: {
-		document.getElementById("flavors-label-title").style.display = "none"
-		return
-	}
+  while (true) {
+    const hasFlavors = document.getElementById("pizzaFlavors");
+    if (!!hasFlavors) {
+      document.getElementById("pizzaFlavors").remove();
+    }
+    if (!hasFlavors) break;
   }
 
-  
-  document.getElementById("flavors-label-title").style.display = "flex"
-  for(let i = 0; numberOfFlavors > i; i++){
-	const flavorsLabel = document.getElementById("flavors");
-	const flavorDiv = document.createElement("div");
+  const pizzaSize = getValueFromDucument("size");
 
-	flavorDiv.id = "pizzaFlavors"
 
-	const flavorSelectElement = document.createElement("select");
-	flavors.forEach((flavor) => {
-		const option = new Option(flavor.name, flavor.value);
-		flavorSelectElement.add(option);
-	});
+  switch (pizzaSize) {
+    case "S": {
+      numberOfFlavors = 2;
+      break;
+    }
+    case "M": {
+      numberOfFlavors = 3;
+      break;
+    }
+    case "L": {
+      numberOfFlavors = 4;
+      break;
+    }
+    case "F": {
+      numberOfFlavors = 5;
+      break;
+    }
+    default: {
+      document.getElementById("flavors-label-title").style.display = "none";
+      return;
+    }
+  }
 
-	const flavorTitle = document.createElement("h5");
-	flavorTitle.innerText = `Sabor ${i + 1}`;
-	flavorDiv.appendChild(flavorTitle);
-	flavorDiv.appendChild(flavorSelectElement);
-	flavorDiv.style.display = "flex";
-	flavorsLabel.style.flexDirection = "column"
-	flavorsLabel.appendChild(flavorDiv);
+  document.getElementById("flavors-label-title").style.display = "flex";
+  for (let i = 0; numberOfFlavors > i; i++) {
+    const flavorsLabel = document.getElementById("flavors");
+    const flavorDiv = document.createElement("div");
+
+    flavorDiv.id = "pizzaFlavors";
+
+    const flavorSelectElement = document.createElement("select");
+    flavorSelectElement.id = `flavor-${i + 1}`;
+    flavors.forEach((flavor) => {
+      const option = new Option(flavor.name, flavor.value);
+      flavorSelectElement.add(option);
+    });
+
+    const flavorTitle = document.createElement("h5");
+    flavorTitle.innerText = `Sabor ${i + 1}`;
+    flavorDiv.appendChild(flavorTitle);
+    flavorDiv.appendChild(flavorSelectElement);
+    flavorDiv.style.display = "flex";
+    flavorsLabel.style.flexDirection = "column";
+    flavorsLabel.appendChild(flavorDiv);
   }
 }
 
 function makeOrder() {
-  const person = getValueFromDucument("name");
-  const email = getValueFromDucument("email");
-  const phone = getValueFromDucument("phone");
+  try {
+    const person = getValueFromDucument("name");
+    const email = getValueFromDucument("email");
+    const phone = getValueFromDucument("phone");
 
-  const street = getValueFromDucument("street");
-  const district = getValueFromDucument("district");
-  const number = getValueFromDucument("number");
-  const description = getValueFromDucument("description");
+    const street = getValueFromDucument("street");
+    const district = getValueFromDucument("district");
+    const number = getValueFromDucument("number");
+    const description = getValueFromDucument("description");
 
-  const size = getValueFromDucument("size");
+    const size = getValueFromDucument("size");
 
-  // const flavors = getValueFromDucument("flavors")
-  const borderFlavor = getValueFromDucument("border-flavor");
+    const flavors = [];
+    for (let i = 0; numberOfFlavors > i; i++) {
+			const select = document.getElementById(`flavor-${i + 1}`);
+      const flavor = select.options[select.selectedIndex].value;
+      if (flavor) flavors.push(flavor);
+    }
 
-  const guarana = getValueFromDucument("guarana", true);
-  const coca = getValueFromDucument("coca-cola", true);
+    const borderFlavor = getValueFromDucument("border-flavor");
 
-  //arrumar isso aq
-  const isDelivery = getValueFromDucument("delivery", true)
+    const guarana = getValueFromDucument("guarana", true);
+    const coca = getValueFromDucument("coca-cola", true);
 
-  const delivery = isDelivery ? true : false
+    const isDelivery = getValueFromDucument("delivery", true);
 
-  const order = {
-	person,
-	email,
-	phone,
-	address: {
-	  street,
-	  district,
-	  number,
-	  description,
-	},
-	size,
-	borderFlavor,
-	drinks: {
-	  guarana,
-	  coca,
-	},
-	delivery,
-  };
-  console.log(order)
-  localStorage.setItem("order", JSON.stringify(order));
+    const delivery = isDelivery ? true : false;
+
+    if (
+      !person ||
+      !email ||
+      !phone ||
+      !street ||
+      !district ||
+      !number ||
+      !size ||
+      !flavors ||
+      !borderFlavor
+    ) {
+      return;
+    }
+
+    const order = {
+      person,
+      email,
+      phone,
+      address: {
+        street,
+        district,
+        number,
+        description,
+      },
+      size,
+      borderFlavor,
+      flavors,
+      drinks: {
+        guarana,
+        coca,
+      },
+      delivery,
+    };
+		localStorage.setItem("order", JSON.stringify(order));
+  } catch (error) {
+		localStorage.setItem("error", JSON.stringify({ error: error }));
+	}
+
 }
